8116d867f09acb23b0f6b23135b6ff6701cf04e2
Nuthelk <46610056+nuthelk@users.noreply.github.com>
2024-06-24 14:20:53 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import path from "path";
 import unzipper from "unzipper";
 import admin from "firebase-admin";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
